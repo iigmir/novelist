72e2d57dc7b6e13537d5b5deb0f9bd2271b747d2
iigmir <roc120j@gmail.com>
2017-10-08 17:06:32 +0800

    Add some novels info

--- db/migrate/20171008075742_create_novels.rb
+++ db/migrate/20171008075742_create_novels.rb
@@ -1,6 +1,9 @@
 class CreateNovels < ActiveRecord::Migration[5.1]
   def change
     create_table :novels do |t|
+      t.string :title
+      t.string :author
+      t.text :plot
 
       t.timestamps
     end
--- db/migrate/20171008075749_create_articles.rb
+++ db/migrate/20171008075749_create_articles.rb
@@ -1,6 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[5.1]
   def change
     create_table :articles do |t|
+      t.integer :novels_id
+      t.integer :chapter
+      t.string :subtitle # For example, "1000: The Death of a Hero" in Q&A
+      t.text :context
 
       t.timestamps
     end
