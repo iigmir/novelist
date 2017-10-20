cdd62bc4ec70d9532774cf1dd36c7554113930d5
iigmir <roc120j@gmail.com>
2017-10-20 17:39:18 +0800

    Do article stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html  
   devise_for :users
-  resources :novels
+  resources :novels do
+    resources :articles
+  end  
   root to: "novels#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008090610) do
+ActiveRecord::Schema.define(version: 20171020090206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
