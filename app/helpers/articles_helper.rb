947e8043c2f2afbaf68ed89cc2aac281e546bbdf
iigmir <roc120j@gmail.com>
2017-11-17 17:47:31 +0800

    Add page navigate

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,15 @@
 module ArticlesHelper
+    def navi_arrow_link( novel_id , article_chapter , oprate_num )
+        arrow_pos = oprate_num == 1 ? "ー＞" : "＜ー"
+        chapter_going = article_chapter + oprate_num
+        a = Article.where( "novel_id = ? and chapter = ?" , novel_id , chapter_going )
+        if a.first != nil
+            link_to arrow_pos, novel_article_path({ novel_id: novel_id , id: chapter_going }) , class:"section"
+        end
+    end
+
+    def navi_calc( novel_id )
+        all_article = Article.where( "novel_id = ?" , novel_id )
+        return all_article.count
+    end
 end
